ff88cfa2ecda3825bf54af0e20eebbd274689717
Jeff Cisneros <cisneros24@gmail.com>
2018-11-21 22:49:30 +0000

    added more features to the services section on homepage, added bubble, pic, titles, paragraphs and unorder list explainin servies provided by South Loop Vet

--- client/my-app/src/components/ServicesHomePage.js
+++ client/my-app/src/components/ServicesHomePage.js
@@ -8,9 +8,9 @@ class ServiceHomePage extends Component {
     
     render() {
           const dogStyle= {
-         'width':'200px',
-         'marginLeft': '5%'
-    };
+         'width':'300px',
+         'marginLeft': '5%',
+         'marginBottom': '3%'    };
     
         
    return ( 
@@ -19,6 +19,18 @@ class ServiceHomePage extends Component {
 Dr. Griffin does great work and genuinely cares for her patients!!! I am so thankful for the addition to our community. Thanks for getting us in at the drop of a hat! 
 </div>
  <img src="dog2.png" style={dogStyle}/>
+ <div id="serviesPara">
+  <h3>Services provided at South Loop Veterinary Clinic</h3>
+   <p id="servicesStatement">We offer comprehensive services for all your animals, to see them through a long, healthy life. <br/> To ensure our patients receive the care they need at every life stage, our services include:</p>
+    <ul>
+    <li>Wellness care & health maintenance</li>
+    <li>Customized vaccination protocols</li>
+    <li>Dental care</li>
+    <li>Surgery</li>
+    <li>Digital X-ray, ECG & in-house diagnostic laboratory testing</li>
+    <li>Emergency care</li>
+    </ul>
+ </div>
        </div>
        
        
